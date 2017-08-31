5d8332de9ffb4af60011e5ad1ae904902399c54c
Billy Fung <billy@billyfung.com>
2017-08-31 17:24:15 +1200

    updating base to have a top bar

--- episode/urls.py
+++ episode/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from episode import views
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^about', views.about)
+    url(r'^$', views.home, name='home_page'),
+    url(r'^about', views.about, name='about_page')
 ]
